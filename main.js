7d6031ec0f9aeac5530c17bee5d9f1f6da1bccf8
unknown <12002655@student.pxl.be>
2024-11-11 04:39:33 +0100

    Eindopdracht opgelost

--- main.js
+++ main.js
@@ -4,12 +4,11 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            SelectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: true },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false },
             ]
         }
     },
@@ -17,8 +16,24 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.SelectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.SelectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.SelectedVariant].quantity
+        },
+        sale() {
+            if (this.variants[this.SelectedVariant].onSale) {
+                return this.title + ' is on sale.'
+            }
         }
     }
 })
